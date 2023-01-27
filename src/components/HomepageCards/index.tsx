2483ec4c2c0e5b2fb6a6303b8bcf1da358a14c4c
Eric Lin <eric.lin@consensys.net>
2023-01-27 11:22:33 +1000

    feat(front page): quick links cards

--- src/components/HomepageCards/index.tsx
+++ src/components/HomepageCards/index.tsx
@@ -1,29 +1,33 @@
 import React from "react";
 import clsx from "clsx";
+import Link from "@docusaurus/Link";
 // import styles from "./styles.module.css";
 
 export default function HomepageCards(): JSX.Element {
   return (
     <section className={clsx("margin-top--lg", "margin-bottom--lg")}>
       <div className="container">
+        <h1>Quick Links</h1>
+        <hr />
         <div className="row">
           <div className={clsx("col col--4")}>
             <div className="card-demo">
               <div className="card">
                 <div className="card__header">
-                  <h3>Lorem Ipsum</h3>
+                  <h3>Getting Started</h3>
                 </div>
                 <div className="card__body">
                   <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                    do eiusmod tempor incididunt ut labore et dolore magna
-                    aliqua. Quis ipsum suspendisse ultrices gravida.
+                    The quickest way to get started by either deploying with
+                    Docker, standalone binary or building from source.
                   </p>
                 </div>
                 <div className="card__footer">
-                  <button className="button button--secondary button--block">
-                    See All
-                  </button>
+                  <Link
+                    className="button button--success button--block"
+                    to="/docs/category/get-started">
+                    Go to guides
+                  </Link>
                 </div>
               </div>
             </div>
@@ -32,19 +36,21 @@ export default function HomepageCards(): JSX.Element {
             <div className="card-demo">
               <div className="card">
                 <div className="card__header">
-                  <h3>Lorem Ipsum</h3>
+                  <h3>Concepts</h3>
                 </div>
                 <div className="card__body">
                   <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                    do eiusmod tempor incididunt ut labore et dolore magna
-                    aliqua. Quis ipsum suspendisse ultrices gravida.
+                    Check out some general concepts that will help you
+                    understand how EthSigner works under the hood and
+                    architected.
                   </p>
                 </div>
                 <div className="card__footer">
-                  <button className="button button--secondary button--block">
-                    See All
-                  </button>
+                  <Link
+                    className="button button--secondary button--block"
+                    to="/docs/category/concepts">
+                    Go to concepts
+                  </Link>
                 </div>
               </div>
             </div>
@@ -53,19 +59,20 @@ export default function HomepageCards(): JSX.Element {
             <div className="card-demo">
               <div className="card">
                 <div className="card__header">
-                  <h3>Lorem Ipsum</h3>
+                  <h3>Reference</h3>
                 </div>
                 <div className="card__body">
                   <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                    do eiusmod tempor incididunt ut labore et dolore magna
-                    aliqua. Quis ipsum suspendisse ultrices gravida.
+                    Find command line arguments, API methods through JSON-RPC,
+                    and general configuration in the References section.
                   </p>
                 </div>
                 <div className="card__footer">
-                  <button className="button button--secondary button--block">
-                    See All
-                  </button>
+                  <Link
+                    className="button button--info button--block"
+                    to="/docs/category/reference">
+                    Go to reference
+                  </Link>
                 </div>
               </div>
             </div>
