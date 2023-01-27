344906b9818a0862f8e23c80b067dd6370ca3b58
Eric Lin <eric.lin@consensys.net>
2023-01-27 13:11:22 +1000

    refactor(frontpage): reactify cards

--- src/components/HomepageCards/index.tsx
+++ src/components/HomepageCards/index.tsx
@@ -3,6 +3,88 @@ import clsx from "clsx";
 import Link from "@docusaurus/Link";
 // import styles from "./styles.module.css";
 
+type CardItem = {
+  title: string;
+  link: string;
+  description: JSX.Element;
+  buttonName: string;
+  buttonType:
+    | "primary"
+    | "secondary"
+    | "success"
+    | "info"
+    | "warning"
+    | "danger"
+    | "link";
+};
+
+const CardList: CardItem[] = [
+  {
+    title: "🏁 Getting Started",
+    link: "string",
+    description: (
+      <>
+        The quickest way to get started by either deploying with Docker,
+        standalone binary or building from source.
+      </>
+    ),
+    buttonName: "Go to guides",
+    buttonType: "success",
+  },
+  {
+    title: "💭 Concepts",
+    link: "string",
+    description: (
+      <>
+        Check out some general concepts that will help you understand how
+        EthSigner works under the hood and architected.
+      </>
+    ),
+    buttonName: "Go to concepts",
+    buttonType: "secondary",
+  },
+  {
+    title: "👨‍💻 Reference",
+    link: "string",
+    description: (
+      <>
+        Find command line arguments, API methods through JSON-RPC, and general
+        configuration in the References section.
+      </>
+    ),
+    buttonName: "Go to reference",
+    buttonType: "info",
+  },
+];
+
+function Card({ title, link, description, buttonName, buttonType }: CardItem) {
+  return (
+    <div className={clsx("col col--4")}>
+      <div className="card-demo">
+        <div className="card">
+          <div className="card__header">
+            <h3>{title}</h3>
+          </div>
+          <div className="card__body">
+            <p>{description}</p>
+          </div>
+          <div className="card__footer">
+            <Link
+              className={clsx(
+                "button",
+                "button--" + buttonType,
+                "button--block",
+              )}
+              to={link}>
+              {buttonName}
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
 export default function HomepageCards(): JSX.Element {
   return (
     <section className={clsx("margin-top--lg", "margin-bottom--lg")}>
@@ -10,76 +92,11 @@ export default function HomepageCards(): JSX.Element {
         <h1>Quick Links</h1>
         <hr />
         <div className="row">
-          <div className={clsx("col col--4")}>
-            <div className="card-demo">
-              <div className="card">
-                <div className="card__header">
-                  <h3>Getting Started</h3>
-                </div>
-                <div className="card__body">
-                  <p>
-                    The quickest way to get started by either deploying with
-                    Docker, standalone binary or building from source.
-                  </p>
-                </div>
-                <div className="card__footer">
-                  <Link
-                    className="button button--success button--block"
-                    to="/docs/category/get-started">
-                    Go to guides
-                  </Link>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className={clsx("col col--4")}>
-            <div className="card-demo">
-              <div className="card">
-                <div className="card__header">
-                  <h3>Concepts</h3>
-                </div>
-                <div className="card__body">
-                  <p>
-                    Check out some general concepts that will help you
-                    understand how EthSigner works under the hood and
-                    architected.
-                  </p>
-                </div>
-                <div className="card__footer">
-                  <Link
-                    className="button button--secondary button--block"
-                    to="/docs/category/concepts">
-                    Go to concepts
-                  </Link>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className={clsx("col col--4")}>
-            <div className="card-demo">
-              <div className="card">
-                <div className="card__header">
-                  <h3>Reference</h3>
-                </div>
-                <div className="card__body">
-                  <p>
-                    Find command line arguments, API methods through JSON-RPC,
-                    and general configuration in the References section.
-                  </p>
-                </div>
-                <div className="card__footer">
-                  <Link
-                    className="button button--info button--block"
-                    to="/docs/category/reference">
-                    Go to reference
-                  </Link>
-                </div>
-              </div>
-            </div>
-          </div>
+          {CardList.map((props, idx) => (
+            <Card key={idx} {...props} />
+          ))}
         </div>
       </div>
-      ;
     </section>
   );
 }
