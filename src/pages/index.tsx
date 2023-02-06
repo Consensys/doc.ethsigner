4ae843f764d0be1a27e5244bd98b7a58ad33be95
Eric Lin <eric.lin@consensys.net>
2023-02-06 10:23:20 +1000

    fix: remove vercel temp and update banner

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,10 +10,19 @@ import styles from "./index.module.css";
 function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext();
   return (
-    <header className={clsx("hero hero--primary", styles.heroBanner)}>
+    <header className={clsx(styles.introductionBlock)}>
       <div className="container">
-        <h1 className="hero__title">{siteConfig.title} 🔑</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
+        <h1 className={clsx("hero__title", styles.forceColor, styles.title)}>
+          {siteConfig.title}
+        </h1>
+        <p
+          className={clsx(
+            "hero__subtitle",
+            styles.forceColor,
+            styles.subtitle,
+          )}>
+          {siteConfig.tagline}
+        </p>
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
